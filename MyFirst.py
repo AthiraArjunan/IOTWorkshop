61cd085deb90aa22f21b4a3a0835d5474686d5e0
AthiraArjunan <athiraarjunan22@gmail.com>
2015-06-04 15:27:34 +0530

    MyFirst.py

---
+++ MyFirst.py
@@ -0,0 +1,12 @@
+print ("enter the 1st no:");
+num1= int(raw_input());
+print ("enter the 2nd no:");
+num2= int(raw_input());
+print ("enter the 3d no:");
+num3= int(raw_input());
+tot=num1+num2+num3;
+if tot==100:
+	 print("success");
+else:
+	print("sad");
+ 	
